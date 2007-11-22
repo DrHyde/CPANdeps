b14e93245ebbf6d5e48fbbc167871d089e590c1f
drhyde <drhyde>
2007-11-22 21:55:45 +0000

    *** empty log message ***

--- getfiles.sh
+++ getfiles.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 cd /web/cpandeps`cat dev_build`.cantrell.org.uk/db
-wget http://cpan.org/modules/02packages.details.txt.gz
+wget -q -O 02packages http://cpan.org/modules/02packages.details.txt.gz &&
+mv 02packages 02packages.details.txt.gz
 
-wget -q http://perl.grango.org/cpanstats.db.gz
+wget -q http://perl.grango.org/cpanstats.db.gz &&
 gzip -d cpanstats.db.gz && (
   ../optimisedb.pl;
   cp cpantestresults cpantestresults.previous;
