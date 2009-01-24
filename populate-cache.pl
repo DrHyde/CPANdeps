532ab49c995f14b1dc9b526ae83ba713e38afdca
drhyde <drhyde>
2009-01-24 14:22:20 +0000

    For debugging

--- populate-cache.pl
+++ populate-cache.pl
@@ -24,6 +24,7 @@ foreach my $file (@files) {
     $file =~ m{^./../([^/]+)(/.*)?/([^/]*).(tar.gz|tgz|zip)$};
     my($author, $dist) = ($1, $3);
     my $local_file  = "db/$dist.yml";
+    print "\n$file\n";
     my $remote_file = "http://search.cpan.org/src/$author/$dist/META.yml";
 
     next if(-e $local_file);
@@ -36,7 +37,7 @@ foreach my $file (@files) {
 	open(FILE, '>', $local_file) || die("Can't write $local_file\n");
 	print FILE $yaml;
 	close(FILE);
-        print "$dist ...\n\t$remote_file -> $local_file\n";
+        print "$dist ...\n\t$remote_file -> \n\t$local_file\n";
 	sleep 1;
     }
 }
