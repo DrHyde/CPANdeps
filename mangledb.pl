91d4057bc71cad5b6e305e4c192a8a801928df34
David Cantrell <david@hetzner.barnyard.co.uk>
2009-06-07 10:25:05 +0100

    Add Midnight BSD

--- mangledb.pl
+++ mangledb.pl
@@ -69,6 +69,7 @@ my %os_by_platform = (
     '%i486-gnu-thread-multi%' => 'GNU Hurd',
     '%osf%'       => 'Tru64/OSF/Digital UNIX',
     '%ARCHREV_0%' => 'HP-UX', # IA64.ARCHREV_0-LP64 / IA64.ARCHREV_0-thread-multi
+    '%midnightbsd%' => 'Midnight BSD',
 );
 foreach my $platform (keys %os_by_platform) {
     print "  $platform -> $os_by_platform{$platform}\n";
