4f4d0eab8db126180738ce917a934775d5baa536
David Cantrell <david@cantrell.org.uk>
2014-04-13 20:40:48 +0100

    update table definitions

--- tables.sql
+++ tables.sql
@@ -1,26 +1,41 @@
-CREATE TABLE cpanstats (
-  id INT NOT NULL PRIMARY KEY,
-  state VARCHAR(16),
-  tester VARCHAR(128),
-  dist VARCHAR(128),
-  version VARCHAR(32),
-  perl VARCHAR(32),
-  is_dev_perl TINYINT DEFAULT 0,
-  os VARCHAR(64),
-  platform VARCHAR(32),
-  origosname VARCHAR(32)
-  -- date INT,              -- not used yet
-  -- osvers VARCHAR(16),    -- not used yet
-  -- arch(VARCHAR(64)       -- not used yet
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-CREATE INDEX perlidx ON cpanstats (perl);
-CREATE INDEX isdevperlidx ON cpanstats (is_dev_perl);
-CREATE INDEX osidx ON cpanstats (os);
-CREATE INDEX distversionidx ON cpanstats (dist, version);
+-- cpandeps database
 
-CREATE TABLE packages (
-  module VARCHAR(128) BINARY NOT NULL PRIMARY KEY,
-  version VARCHAR(32),
-  file VARCHAR(256)
+CREATE TABLE `cpanstats` (
+  `id` int(11) NOT NULL,
+  `state` varchar(16) DEFAULT NULL,
+  `tester` varchar(128) DEFAULT NULL,
+  `dist` varchar(128) DEFAULT NULL,
+  `version` varchar(32) DEFAULT NULL,
+  `perl` varchar(32) DEFAULT NULL,
+  `is_dev_perl` tinyint(4) DEFAULT '0',
+  `os` varchar(64) DEFAULT NULL,
+  `platform` varchar(32) DEFAULT NULL,
+  `origosname` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `perlidx` (`perl`),
+  KEY `isdevperlidx` (`is_dev_perl`),
+  KEY `osidx` (`os`),
+  KEY `distversionidx` (`dist`,`version`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE `packages` (
+  `module` varchar(128) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `version` varchar(32) DEFAULT NULL,
+  `file` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`module`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- cpantesters database
+
+CREATE TABLE `cpanstats` (
+  `id` int(11) NOT NULL,
+  `guid` varchar(32) DEFAULT NULL,
+  `state` varchar(16) DEFAULT NULL,
+  `dist` varchar(128) DEFAULT NULL,
+  `version` varchar(32) DEFAULT NULL,
+  `perl` varchar(32) DEFAULT NULL,
+  `platform` varchar(32) DEFAULT NULL,
+  `osname` varchar(64) DEFAULT NULL,
+  `osvers` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
