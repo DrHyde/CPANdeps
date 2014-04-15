ab2fe893032f5d4d4fee3c0e487a0631117dba6b
David Cantrell <david@cantrell.org.uk>
2014-04-15 21:43:40 +0100

    cache the fact that we couldn't find a META.yml/json

--- populate-cache.pl
+++ populate-cache.pl
@@ -47,20 +47,23 @@ foreach my $file (@files) {
     my($author, $dist) = ($1, $3);
     next if(!defined($author) || !defined($dist));
 
+    next if(
+        -e "db/META.yml/$dist.yml" || -e "db/META.yml/$dist.json" ||
+        (-e "db/META.yml/$dist.yml.404" && -e "db/META.yml/$dist.json.404")
+    );
     foreach my $tuple (
         { local => "db/META.yml/$dist.yml", remote => "http://search.cpan.org/src/$author/$dist/META.yml" },
         { local => "db/META.yml/$dist.json", remote => "http://search.cpan.org/src/$author/$dist/META.json" },
     ) {
         my $local_file  = $tuple->{local};
         my $remote_file = $tuple->{remote};
-
-        next if(-e $local_file);
   
         print '.';
 
         my $res = $ua->request(HTTP::Request->new(GET => $remote_file));
         if(!$res->is_success()) {
-            next;
+            open(FILE, '>', "$local_file.404") || die("Can't write $local_file.404\n");
+            close(FILE);
         } else {
           my $yaml = $res->content();
           open(FILE, '>', $local_file) || die("Can't write $local_file\n");
