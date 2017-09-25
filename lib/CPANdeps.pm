a3930c7f93c2692ce398bd91e5c8ac0166682a5c
David Cantrell <david@cantrell.org.uk>
2017-09-25 13:58:14 +0100

    bump LATESTPERL

--- lib/CPANdeps.pm
+++ lib/CPANdeps.pm
@@ -21,7 +21,7 @@ use Template;
 
 use constant ANYVERSION => 'any version';
 use constant ANYOS      => 'any OS';
-use constant LATESTPERL  => '5.26.0';
+use constant LATESTPERL  => '5.26.1';
 use constant DEFAULTCORE => '5.005';
 use constant MAXINT => ~0;
 
