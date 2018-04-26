9c2832b3de8565e14cf3ff4b50d64c5211851f78
David Cantrell <david@cantrell.org.uk>
2018-04-26 12:43:31 +0100

    5.26.2 is out; 5.28 is imminent; 5.27 has had many releases

--- lib/CPANdeps.pm
+++ lib/CPANdeps.pm
@@ -22,7 +22,7 @@ use Template;
 
 use constant ANYVERSION => 'any version';
 use constant ANYOS      => 'any OS';
-use constant LATESTPERL  => '5.26.1';
+use constant LATESTPERL  => '5.26.2';
 use constant DEFAULTCORE => '5.005';
 use constant MAXINT => ~0;
 
