9e528c50d008473d374a8425d81b90fa747c4d5d
David Cantrell <david@cantrell.org.uk>
2018-06-28 22:46:37 +0100

    bump LATERPERL to 5.28.0

--- mangledb.pl
+++ mangledb.pl
@@ -102,7 +102,7 @@ SELECTLOOP:
   $cpandepsdbh->{'AutoCommit'} = 0;
   while(my $record = $select->fetchrow_hashref()) {
     # NB the order of these two lines is important!
-    $record->{is_dev_perl} = ($record->{perl} =~ /(^5\.(7|9|11|13|15|17|19|21|23|25|27|29)|rc|patch)/i) ? 1 : 0;
+    $record->{is_dev_perl} = ($record->{perl} =~ /(^5\.(7|9|11|13|15|17|19|21|23|25|27|29|31)|rc|patch)/i) ? 1 : 0;
     $record->{perl} =~ s/\s+RC\d+//i;
     # NB this deliberately skips 5.18.3.
     # See http://www.nntp.perl.org/group/perl.perl5.porters/2014/10/msg220771.html
@@ -131,6 +131,8 @@ SELECTLOOP:
         (map { "5.26.$_" } (0 .. 10)),
         (map { "5.27.$_" } (0 .. 11)),
         (map { "5.28.$_" } (0 .. 10)),
+        (map { "5.29.$_" } (0 .. 10)),
+        (map { "5.30.$_" } (0 .. 10)),
     ) {
       $record->{perl} = $ver if($record->{perl} =~ /^v?$ver/);
     }
